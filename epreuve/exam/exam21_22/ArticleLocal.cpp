d14f9327c6b75a7d81b9254814b6724a6739ecaa
Ahmadoubambamb <121469769+Ahmadoubambamb@users.noreply.github.com>
2024-09-15 01:43:08 +0000

    Update ArticleLocal.cpp

--- epreuve/exam/exam21_22/ArticleLocal.cpp
+++ epreuve/exam/exam21_22/ArticleLocal.cpp
@@ -5,14 +5,13 @@ ArticleLocal::ArticleLocal(const string& num,const string& d,double prix,double 
     cout_transp=cout;
 }
 
-ostream& operator<<(ostream& out,const ArticleLocal& al)
+ostream& ArticleLocal::afficher(ostream& out) const
 {
-  out<<"numero de reference: "<<al.num_reference<<endl
-     <<"designation: "<<al.designation<<endl
-     <<"prix d'achat: "<<al.prix_achat<<endl
-     <<"cout du transport: "<<al.cout_transp<<endl;
+     Article::afficher(out);
+     out<<"cout du transport: "<<cout_transp<<endl;
    return out;
 }
+
 
 double ArticleLocal::prix()
 {
